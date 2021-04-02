63c03a9831af8563b4707a91974b8478217dd10a
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-04-02 13:21:58 -0400

    Added error messages to input fields
    if the username or email already exists an error message will show up below the input field. Along with error an error message showing up below the email field for an invalid email format and the confirm password field if the passwords do not match.

--- script/register_script.js
+++ script/register_script.js
@@ -3,14 +3,14 @@
  * This script will check whether or not a user registering for an account on a website is a new user. This is achieved by using
  * local storage to act as a database to search for existing accounts.
  * If the username or email address they entered already exists, it will notify them. 
- * If they were successfully able to register their account they will be brought back to the login page
+ * If they were successfully able to register their account they will be brought to the profile page
  * 
  */
 
 
 import User from './module/user.js';
 
-//let buttonDisabled = true;
+
 let firstnameFilled;
 let lastnameFilled;
 let usernameFilled;
@@ -25,6 +25,19 @@ let username = document.getElementById('username-field');
 let password = document.getElementById('password-field');
 let confirmPassword = document.getElementById('confirm-password-field');
 
+let usernameErrorMessage = document.getElementById('username-error-message');
+usernameErrorMessage.style.display = "none";
+
+let emailErrorMessage = document.getElementById('email-error-message');
+emailErrorMessage.style.display = "none";
+
+let emailErrorMessage2 = document.getElementById('email-error-message2');
+emailErrorMessage2.style.display = "none";
+
+let passwordErrorMessage = document.getElementById('password-error-message');
+passwordErrorMessage.style.display = "none";
+
+
 let submitBtn = document.getElementById('submit-button');
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -35,29 +48,29 @@ submitBtn.disabled = true;
 submitBtn.classList.add("disabled");
 
 //will constatnly check to see if anything is typed into the field
-firstname.addEventListener("input", (event)=>{
-     firstnameFilled = checkInputField(firstname);
-     
+firstname.addEventListener("input", (event) => {
+    firstnameFilled = checkInputField(firstname);
+
 });
 
 lastname.addEventListener("input", () => {
-     lastnameFilled = checkInputField(lastname);
+    lastnameFilled = checkInputField(lastname);
 });
 
 email.addEventListener("input", () => {
-    emailFilled =  checkInputField(email);
+    emailFilled = checkInputField(email);
 });
 
 username.addEventListener("input", () => {
-     usernameFilled = checkInputField(username);
+    usernameFilled = checkInputField(username);
 });
 
 password.addEventListener("input", () => {
-     passwordFilled = checkInputField(password);
+    passwordFilled = checkInputField(password);
 });
 
 confirmPassword.addEventListener("input", () => {
-     confirmPasswordFilled = checkInputField(confirmPassword);
+    confirmPasswordFilled = checkInputField(confirmPassword);
 });
 
 
@@ -66,14 +79,14 @@ confirmPassword.addEventListener("input", () => {
 //a loop that will continually check if all fields of the form are filled every 500ms
 var interval = 500;
 window.setInterval(() => {
-    // your code goes here ...
-    if(firstnameFilled && lastnameFilled && usernameFilled && passwordFilled && confirmPasswordFilled) {
+
+    if (firstnameFilled && lastnameFilled && usernameFilled && passwordFilled && confirmPasswordFilled) {
         submitBtn.disabled = false;
         submitBtn.classList.remove("disabled");
-        console.log("hello");
-    
+
+
     }
-    else{
+    else {
         submitBtn.disabled = true;
         submitBtn.classList.add('disabled');
     }
@@ -95,8 +108,24 @@ function registerFunction() {
     confirmPassword = document.getElementById('confirm-password-field').value;
 
 
+    if (!validateUsernameRegistration(username)) {
+        usernameErrorMessage.style.display = "block";
+    }
 
-    if (password === confirmPassword && validateRegistration(username, email)) {
+    if (!validateEmailRegistration(email)) {
+        emailErrorMessage.style.display = "block";
+    }
+
+    console.log(validEmailFormat(email));
+    if (!validEmailFormat(email)) {
+        emailErrorMessage2.style.display = "block";
+    }
+
+    if(password !== confirmPassword){
+        passwordErrorMessage.style.display = "block";
+    }
+
+    if (password === confirmPassword && validateUsernameRegistration(username) && validateEmailRegistration(email) && validEmailFormat(email)) {
         var newUser = new User(firstname, lastname, email, username, password);
         console.log(newUser);
 
@@ -109,45 +138,72 @@ function registerFunction() {
         window.location.href = "profile.html";
     }
 
-    else {
-        console.log("username already exists");
-    }
-
 }
 
 /**
  * 
  * @param {*} unregUsername string from the username input field 
- * @param {*} unregEmail string from the email input field
  * @returns a boolean value depending on whether the email and username inputted are unregistered. True if no registered, false if 
  *          they alread exist
  */
-function validateRegistration(unregUsername, unregEmail) {
+function validateUsernameRegistration(unregUsername) {
 
-    if (localStorage.getItem(unregUsername) === null && localStorage.getItem(unregEmail) == null) {
+    if (localStorage.getItem(unregUsername) === null) {
         return true;
     }
 
     return false;
 }
 
+/**
+ * 
+ * @param {*} unregEmail string from the email input field
+ * @returns a boolean value depending on whether the email and username inputted are unregistered. True if no registered, false if 
+ *          they alread exist
+ */
+function validateEmailRegistration(unregEmail) {
 
-function checkInputField(inputField){
-    
-    const notEmpty = inputField.value;
-    const notEmptySpaces = inputField.value.trim();
-    if(notEmpty && notEmptySpaces){
-        //buttonDisabled = false;
-        //submitBtn.disabled = false;
-        //submitBtn.classList.remove("disabled");
+    if (localStorage.getItem(unregEmail) == null) {
         return true;
     }
 
-    // else{
-    //     buttonDisabled = true;
-    //     //submitBtn.disabled = true;
-    //     submitBtn.classList.add("disabled")
-    // }
+    return false;
+}
 
+function validPasswordFormat(passwordField) {
+    let array = passwordField.split("");
+    let validPassword = true;
+    array.forEach((i) => {
+        if (i === ' ') {
+            validPassword = false;
+        }
+        else {
+            validPassword = true;
+        }
+    });
+    return validPassword;
+}
+
+function validEmailFormat(emailField) {
+
+    if (emailField.indexOf('@') != -1) {
+        console.log("no @");
+        let array = emailField.substring(emailField.indexOf("@"));
+        array = array.split('');
+        console.log(array);
+        if (array.length > 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkInputField(inputField) {
+
+    const notEmpty = inputField.value;
+    const notEmptySpaces = inputField.value.trim();
+    if (notEmpty && notEmptySpaces) {
+        return true;
+    }
     return false;
 }
