c77c3783706194ef7e8779e1186ba800fb6f28a8
SunWukong97 <34043095+SunWukong97@users.noreply.github.com>
2021-04-14 19:37:01 -0400

    added email validation
    if the email address is in a valid format and is a registered one an alert will pop up showing what the password for the account was. If the email is in a valid format but not registered an alert banner will just show up indicating an email has been sent.

--- script/forgot_password_script.js
+++ script/forgot_password_script.js
@@ -1,27 +1,28 @@
-let modalContainer = document.getElementById("modal-container");
+let alertBannerContainer = document.getElementById("alert-banner-container");
 let errorMessage = document.getElementById("email-field-error-message");
 let emailField = document.getElementById("email-field");
 
+let password;
 
 
 document.getElementById("next-button").addEventListener('click', (event) => {
     event.preventDefault();
     next();
 });
-    
 
-
-
-
-
-function next(){
-    if(!validEmailFormat(emailField.value)){
-        errorMessage.style.display = "block";
+/**
+ * function for the next button, will display a banner at the top of the page if everything was 
+ * successful
+ */
+function next() {
+    validEmailFormat(emailField.value) ? errorMessage.style.display = "none" : errorMessage.style.display = "block";
+    if (validateRegisteredEmail(emailField.value)) {
+        alertBannerContainer.classList.add("alert-banner-container-animation");
+        window.alert(`shhh... the password for ${emailField.value} was: ${password}`);
     }
-    
-    else{
-        errorMessage.style.display = "none";
-        modalContainer.classList.add("modal-animation"); 
+
+    if (!validateRegisteredEmail(emailField.value) && (validEmailFormat(emailField.value))){
+        alertBannerContainer.classList.add("alert-banner-container-animation");
     }
 }
 
@@ -31,12 +32,33 @@ function next(){
  * @param {*} this_emailField string from the email input field
  * @returns a boolean value depending if the email is formatted correctly 
  */
- function validEmailFormat(this_emailField) {
+function validEmailFormat(this_emailField) {
 
-    if(this_emailField.indexOf('@') != -1){
+    if (this_emailField.indexOf('@') != -1) {
         let array = this_emailField.substring(this_emailField.indexOf('@'));
         array = array.split('');
-        if(array.length > 1){
+        if (array.length > 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Checks to see if the email address is one that is registered
+ * @param {*} emailAddress string from the email input field 
+ * @returns a boolean value if the email address is registered. True if the email is registerd, false if it isn't.
+ */
+function validateRegisteredEmail(emailAddress) {
+
+    for (var i = 0; i < localStorage.length; i++) {
+
+        var key = localStorage.key(i);
+        var accountInfo = JSON.parse(localStorage.getItem(key));
+
+        if (emailAddress == accountInfo.email) {
+            password = accountInfo.password;
+
             return true;
         }
     }
